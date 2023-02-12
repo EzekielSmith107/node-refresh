6ff0cc70b837e74d3f379208fda612181bdfe7e9
Ezekiel Smith <ezesmith107@gmail.com>
2023-02-12 13:48:05 -0500

    added successful db exchange

--- public/index.js
+++ public/index.js
@@ -1,7 +1,22 @@
+const monsterContainer = document.getElementById("monster-container");
+
 function generateMonster() {
   axios.get("http://localhost:4000/api/monster/").then((res) => {
-    console.log(res);
+    console.log(res.data);
+    createProfile(res.data);
   });
+}
+
+function createProfile(data) {
+  const monsterCard = document.createElement("div");
+  monsterCard.classList.add("monster-card");
+
+  console.log(data.name);
+  console.log(data.imageUrl);
+  monsterCard.innerHTML = `<img alt=${data.name} src=${data.imageUrl} />
+  <p class='monster-name'>${data.name}</p>`;
+
+  monsterContainer.appendChild(monsterCard);
 }
 
 const magicButton = document.getElementById("magic-button");
--- server/controller.js
+++ server/controller.js
@@ -1,19 +1,9 @@
 const monsters = require("./db.json");
 
 module.exports = {
-  // generateMonster: (req, res) => {
-  //   const message = "All good!";
-  //   console
-  //     .log(monsters)
-  //     .then(() => {
-  //       console.log(message);
-  //       res.sendStatus(200);
-  //     })
-  //     .catch((err) => console.log(err));
-  // },
-
   generateMonster: (req, res) => {
-    console.log(monsters);
-    res.status(200).send(monsters);
+    let monsterIndex = Math.floor(Math.random() * monsters.length);
+    let monster = monsters[monsterIndex];
+    res.status(200).send(monster);
   },
 };
